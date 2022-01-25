94ff23e194b5a5ce469cbe8881648be3aae2c0af
Drozdzu333 <drozdzu333@gmail.com>
2022-01-25 14:46:29 +0100

    Feature replace board values to string array.

--- Tic Tac Toe/Class1.cs
+++ Tic Tac Toe/Class1.cs
@@ -8,6 +8,7 @@ namespace Tic_Tac_Toe
 {
     public class Board
     {
+
         char A1 = 'O';
         char A2 = 'O';
         char A3 = 'O';
@@ -17,14 +18,23 @@ namespace Tic_Tac_Toe
         char C1 = 'O';
         char C2 = 'O';
         char C3 = 'O';
-        public Board()
+        public Board(string[] marks)
         {
-             Console.WriteLine("    A   B    C");
-            Console.WriteLine($"1   {A1} | {B1} | {C1}");
-             Console.WriteLine("________________");
-            Console.WriteLine($"1   {A2} | {B2} | {C2}");
-             Console.WriteLine("________________");
-            Console.WriteLine($"1   {A3} | {B3} | {C3}");
+            //string[] Marks = marks;
+
+            //Console.WriteLine("    A   B    C");
+            //Console.WriteLine($"1   {A1} | {B1} | {C1}");
+            // Console.WriteLine("________________");
+            //Console.WriteLine($"1   {A2} | {B2} | {C2}");
+            // Console.WriteLine("________________");
+            //Console.WriteLine($"1   {A3} | {B3} | {C3}");
+
+            Console.WriteLine("    A   B    C");
+            Console.WriteLine($"1   {marks[0]} | {marks[1]} | {marks[2]}");
+            Console.WriteLine("________________");
+            Console.WriteLine($"1   {marks[0]} | {marks[0]} | {marks[0]}");
+            Console.WriteLine("________________");
+            Console.WriteLine($"1   {marks[0]} | {marks[0]} | {marks[0]}");
         }
     }
 }
