c818442139008832c2730e4a7cf8fccd6bb1f46d
Drozdzu333 <drozdzu333@gmail.com>
2022-01-25 12:21:06 +0100

    Create class board with variables in board spaces.

---
+++ Tic Tac Toe/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    public class Board
+    {
+        char A1 = 'O';
+        char A2 = 'O';
+        char A3 = 'O';
+        char B1 = 'O';
+        char B2 = 'O';
+        char B3 = 'O';
+        char C1 = 'O';
+        char C2 = 'O';
+        char C3 = 'O';
+        public Board()
+        {
+             Console.WriteLine("    A   B    C");
+            Console.WriteLine($"1   {A1} | {B1} | {C1}");
+             Console.WriteLine("________________");
+            Console.WriteLine($"1   {A2} | {B2} | {C2}");
+             Console.WriteLine("________________");
+            Console.WriteLine($"1   {A3} | {B3} | {C3}");
+        }
+    }
+}
--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Tic_Tac_Toe;
+
+Console.WriteLine("Hello, World!\nWelcome to console game Tic Tac Toe developed by Michał Rożdżyński");
+Console.WriteLine("This game is stering by coordinates writen in to the console.");
+Board board = new Board();
+
+Console.Read();
